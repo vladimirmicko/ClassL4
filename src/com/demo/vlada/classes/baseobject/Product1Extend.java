cf65e1122eaa549dd9d5ebc23129b6d14896c335
Vladimir Randjelovic <vladimirmicko@gmail.com>
2016-07-29 14:06:28 +0200

    Dodate klase Product1Extend i Product1Override

---
+++ src/com/demo/vlada/classes/baseobject/Product1Extend.java
@@ -0,0 +1,6 @@
+package com.demo.vlada.classes.baseobject;
+
+public class Product1Extend extends Product1{
+
+	
+}
---
+++ src/com/demo/vlada/classes/baseobject/Product1Override.java
@@ -0,0 +1,11 @@
+package com.demo.vlada.classes.baseobject;
+
+public class Product1Override extends Product1 {
+
+	@Override
+	public int calculate(int a, int b) {
+		return a-b;
+	}
+	
+
+}
